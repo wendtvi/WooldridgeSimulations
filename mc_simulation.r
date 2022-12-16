d42a5472d0609bf3e4e9fbe0b056469afc4632fd
wendtvi <vitoriawendt@gmail.com>
2022-12-16 17:10:34 -0300

    Update mc_simulation.r

--- mc_simulation.r
+++ mc_simulation.r
@@ -4,6 +4,19 @@
 #install.packages("devtools")
 library(devtools)
 
-N=1000
+source_url("https://github.com/wendtvi/dissertation_code/blob/main/continous_nonlinear.r?raw=TRUE")
 
-source_url("")
+matriz_resultados=mc_function(N=1000)
+vetor_mc_resultados_vies=vector()
+vetor_mc_resultados_sd=vector()
+vetor_mc_resultados_media_pop=vector()
+vetor_mc_resultados_media_est=vector()
+c=0
+for (k in seq(1,ncol(matriz_resultados),2)){
+  c=c+1
+  vetor_mc_resultados_vies[c]=mean(matriz_resultados[,k])-mean(matriz_resultados[,k+1])
+  vetor_mc_resultados_sd[c]=sd(matriz_resultados[,k+1])
+  vetor_mc_resultados_media_pop[c]=mean(matriz_resultados[,k])
+  vetor_mc_resultados_media_est[c]=mean(matriz_resultados[,k+1])
+}
+
