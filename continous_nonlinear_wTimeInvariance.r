840a88fb9bee9e5c12b90c8fbc1b7e1f99521e40
wendtvi <vitoriawendt@gmail.com>
2022-12-16 18:10:54 -0300

    Update continous_nonlinear_wTimeInvariance.r

--- continous_nonlinear_wTimeInvariance.r
+++ continous_nonlinear_wTimeInvariance.r
@@ -23,23 +23,23 @@ mc_function=function(N){
       sum(D)/length(D) #Incidência de tratamento na população
       
       #Termos de erro U
-      U0=rnorm(n1,0,1)
-      U1=rnorm(n1,0,1)
+      U0=rnorm(n1,0,1.81)
+      U1=rnorm(n1,0,1.81)
       
       #Modelo de diferenças em diferença para cada cohort (01 primero é grupo e segundo é tempo: neste caso é grupo de controle no período pós trat)
       f5=rep(1,n1)
       f6=rep(1,n1)
-      X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U1 #para t=1,...,TT caso não houvesse tratamento
+      X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81) #para t=1,...,TT caso não houvesse tratamento
       X_estrela_1_ntratados=X_estrela_0 #se houvesse tratamento, mas var observada antes do tratamento
-      X_estrela_1_t4=(Z_cov_mean-1)*3/4-2*D+U1 #para t=4
-      X_estrela_1_t5=(Z_cov_mean-1)*3/4-2*D+0.2*f5*D+U1 #para t=5
-      X_estrela_1_t6=(Z_cov_mean-1)*3/4-2*D+0.2*f5*D+0.3*f6*D+U1 #para t=6
+      X_estrela_1_t4=(Z_cov_mean-1)*3/4-2*D+rnorm(n1,0,1.81) #para t=4
+      X_estrela_1_t5=(Z_cov_mean-1)*3/4-2*D+0.2*f5*D+rnorm(n1,0,1.81) #para t=5
+      X_estrela_1_t6=(Z_cov_mean-1)*3/4-2*D+0.2*f5*D+0.3*f6*D+rnorm(n1,0,1.81) #para t=6
         
       matriz_estado_naotratamento=matrix(NA,nrow = n1,ncol = TT+1)
       matriz_estado_naotratamento[,TT+1]=t(D)
       for (k in 1:TT){
-        if (k<q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U0
-        if (k>=q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U0
+        if (k<q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
+        if (k>=q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
         matriz_estado_naotratamento[,k]=t(X_estrela_0)
       }
       
@@ -61,20 +61,20 @@ mc_function=function(N){
           matriz_Y[i,k]=exp(matriz_X_estrela[i,k])
         }
       }
-      hist(matriz_Y[matriz_Y[,TT+1]==0,q-1],breaks = 200,main="Histograma grupo de controle em t=3 (período pré tratamento)")
+      #hist(matriz_Y[matriz_Y[,TT+1]==0,q-1],breaks = 200,main="Histograma grupo de controle em t=3 (período pré tratamento)")
       
       ##########################################################
       #####################ESTIMADORES##########################
       ##########################################################
       Y10= matriz_Y[matriz_Y[,TT+1]==1,(q-1)] #variável resposta observada para grupo dos tratados no período pré tratamento t=3
       #Suponho que sei que variável latente segue distribuição logistica com parâmetros 0,1
-      F_Y10=pnorm(Y10,mean=(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)),sd = 1)
+      F_Y10=plnorm((Y10),meanlog =exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2),
+                   sdlog =sqrt((exp(1.81^2)-1)*exp(2*exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2)+(1.81)^2)))
       F_Y10[F_Y10==1]=0.9999999999
-      hist(F_Y10)
-      F_inver_F_Y10=qnorm(F_Y10,mean = (mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)),sd = 1 )
-      plot(F_inver_F_Y10)
-      
-      
+      #hist(F_Y10)
+      F_inver_F_Y10=qlnorm(F_Y10,meanlog =exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2),
+                           sdlog = sqrt((exp(1.81^2)-1)*exp(2*exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2)+(1.81)^2)))
+  
       
       ##########################################################
       #####################RESULTADOS###########################
