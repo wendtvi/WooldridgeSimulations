37ee316aee9e9038473835022f68c8b4f599f9cb
wendtvi <vitoriawendt@gmail.com>
2022-12-16 16:17:06 -0300

    Update and rename continous_nonlinear.r to continous_nonlinear_wTimeInvariance.r

--- continous_nonlinear_wTimeInvariance.r
+++ continous_nonlinear_wTimeInvariance.r
@@ -5,7 +5,6 @@ mc_function=function(N){
 #####################CENÁRIO SIMULAÇÃO####################
 ##########################################################
       n1=500
-      n0=500
       TT=6
       q=4
       
@@ -30,20 +29,28 @@ mc_function=function(N){
       #Modelo de diferenças em diferença para cada cohort (01 primero é grupo e segundo é tempo: neste caso é grupo de controle no período pós trat)
       f5=rep(1,n1)
       f6=rep(1,n1)
-      X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U0 #para t=1,...,TT
-      X_estrela_1_ntratados=rep(0.5,n0)+(Z_cov_mean-1)+U1 #nao tratados após tratamento
+      X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U1 #para t=1,...,TT caso não houvesse tratamento
+      X_estrela_1_ntratados=X_estrela_0 #se houvesse tratamento, mas var observada antes do tratamento
       X_estrela_1_t4=rep(0.5,n1)+(Z_cov_mean-1)-2*D+U1 #para t=4
-      X_estrela_1_t5=rep(0.5,n1)+(Z_cov_mean-1)-2*D+0.2*f5+U1 #para t=5
-      X_estrela_1_t6=rep(0.5,n1)+(Z_cov_mean-1)-2*D+0.2*f5+0.3*f6+U1 #para t=6
+      X_estrela_1_t5=rep(0.5,n1)+(Z_cov_mean-1)-2*D+0.2*f5*D+U1 #para t=5
+      X_estrela_1_t6=rep(0.5,n1)+(Z_cov_mean-1)-2*D+0.2*f5*D+0.3*f6*D+U1 #para t=6
         
+      matriz_estado_naotratamento=matrix(NA,nrow = n1,ncol = TT+1)
+      matriz_estado_naotratamento[,TT+1]=t(D)
+      for (k in 1:TT){
+        if (k<q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U0
+        if (k>=q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+U0
+        matriz_estado_naotratamento[,k]=t(X_estrela_0)
+      }
+      
       #Gerando matriz de dados latentes.
-      matriz_X_estrela=matrix(NA, ncol = TT+1,nrow = n1+n0)
-      matriz_X_estrela[,TT+1]=t(c(rep(1,n1),rep(0,n0)))
+      matriz_X_estrela=matrix(NA, ncol = TT+1,nrow = n1)
+      matriz_X_estrela[,TT+1]=t(D)
       for (k in 1:ncol(matriz_X_estrela)-1){
-        matriz_X_estrela[,k]=t(c(X_estrela_0,X_estrela_0))
-        if (k==4)matriz_X_estrela[,k]=t(c(X_estrela_1_t4,X_estrela_1_ntratados))
-        if (k==5)matriz_X_estrela[,k]=t(c(X_estrela_1_t5,X_estrela_1_ntratados))
-        if (k==6)matriz_X_estrela[,k]=t(c(X_estrela_1_t6,X_estrela_1_ntratados))
+        matriz_X_estrela[,k]=matriz_estado_naotratamento[,k]
+        if (k==4)matriz_X_estrela[,k]=t(c(X_estrela_1_t4))
+        if (k==5)matriz_X_estrela[,k]=t(c(X_estrela_1_t5))
+        if (k==6)matriz_X_estrela[,k]=t(c(X_estrela_1_t6))
       }
       
       #Transformação W() apenas replica X estrela
@@ -61,9 +68,9 @@ mc_function=function(N){
       ##########################################################
       Y10= matriz_Y[matriz_Y[,TT+1]==1,(q-1)] #variável resposta observada para grupo dos tratados no período pré tratamento t=3
       #Suponho que sei que variável latente segue distribuição logistica com parâmetros 0,1
-      F_Y10=plogis(Y10,location=mean((Z_cov_mean-1)/2),scale = 1)
+      F_Y10=plogis(Y10,location=(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)),scale = 1)
       hist(F_Y10)
-      F_inver_F_Y10=qlogis(F_Y10,location = mean(rep(0.5,n1)+(Z_cov_mean-1)),scale = 1 )
+      F_inver_F_Y10=qlogis(F_Y10,location = (mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)),scale = 1 )
       plot(F_inver_F_Y10)
       
       
@@ -71,13 +78,13 @@ mc_function=function(N){
       ##########################################################
       #####################RESULTADOS###########################
       ##########################################################
-      tau_4=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q])-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q]) #resultados populacionais
-      tau_5=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+1])-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+1])
-      tau_6=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+2])-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+2])
+      tau_4=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q])-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,q]) #resultados populacionais
+      tau_5=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+1])-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,q+1])
+      tau_6=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+2])-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,q+2])
       
-      tau_4_hat=mean(log(matriz_Y[matriz_Y[,TT+1]==1,q]))-mean(log(F_inver_F_Y10[F_inver_F_Y10!=Inf]))
-      tau_5_hat=mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+1]))-mean(log(F_inver_F_Y10[F_inver_F_Y10!=Inf]))
-      tau_6_hat=mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+2]))-mean(log(F_inver_F_Y10[F_inver_F_Y10!=Inf]))
+      tau_4_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q])))/exp(mean(log(F_inver_F_Y10))))
+      tau_5_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+1])))/exp(mean(log(F_inver_F_Y10))))
+      tau_6_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+2])))/exp(mean(log(F_inver_F_Y10))))
       
       matriz_resultados[p,1]=tau_4
       matriz_resultados[p,2]=tau_4_hat
