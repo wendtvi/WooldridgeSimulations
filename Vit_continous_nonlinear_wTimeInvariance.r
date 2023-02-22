47c511022ae72654c8db6e2897171dcebaeb6460
wendtvi <vitoriawendt@gmail.com>
2023-02-22 15:24:21 -0300

    Update Vit_continous_nonlinear_wTimeInvariance.r

--- Vit_continous_nonlinear_wTimeInvariance.r
+++ Vit_continous_nonlinear_wTimeInvariance.r
@@ -1,8 +1,8 @@
 mc_function=function(N){
-  n1=500
+  n1=1000
   TT=6
   q=4
-  matriz_resultados=matrix(NA, ncol = (TT-q+1)*2,nrow = N)
+  matriz_resultados=matrix(NA, ncol = 2*((TT-q+1)*2),nrow = N)
   
   for(p in 1:N){
     ##########################################################
@@ -39,20 +39,29 @@ mc_function=function(N){
     matriz_estado_naotratamento=matrix(NA,nrow = n1,ncol = TT+1)
     matriz_estado_naotratamento[,TT+1]=t(D)
     for (k in 1:TT){
-      if (k<q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
-      if (k>=q) X_estrela_0=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
-      matriz_estado_naotratamento[,k]=t(X_estrela_0)
+      if (k<q) matriz_estado_naotratamento[,k]=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
+      if (k>=q) matriz_estado_naotratamento[,k]=(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1.81)
     }
+    
     
     #Gerando matriz de dados latentes.
     matriz_X_estrela=matrix(NA, ncol = TT+1,nrow = n1)
     matriz_X_estrela[,TT+1]=t(D)
     for (k in 1:ncol(matriz_X_estrela)-1){
-      matriz_X_estrela[,k]=matriz_estado_naotratamento[,k]     
+      matriz_X_estrela[,k]=matriz_estado_naotratamento[,k]      
       for (i in 1:nrow(matriz_X_estrela)){
         if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=X_estrela_1_t4[i]
         if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=X_estrela_1_t5[i]
         if (k==6 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=X_estrela_1_t6[i]
+      }
+    }
+    
+    #Transformação W() apenas replica X estrela matriz de não tratamento
+    #Tranformação não linear S() exp()
+    matriz_Y_naotratamento=matriz_estado_naotratamento
+    for (k in 1:ncol(matriz_Y_naotratamento)-1){
+      for (i in 1:nrow(matriz_Y_naotratamento)){
+        matriz_Y_naotratamento[i,k]=exp(matriz_Y_naotratamento[i,k])
       }
     }
     
@@ -64,20 +73,6 @@ mc_function=function(N){
         matriz_Y[i,k]=exp(matriz_X_estrela[i,k])
       }
     }
-    #hist(matriz_Y[matriz_Y[,TT+1]==0,q-1],breaks = 200,main="Histograma grupo de controle em t=3 (período pré tratamento)")
-    
-    ##########################################################
-    #####################ESTIMADORES##########################
-    ##########################################################
-    Y10= matriz_Y[matriz_Y[,TT+1]==1,(q-1)] #variável resposta observada para grupo dos tratados no período pré tratamento t=3
-    #Suponho que sei que variável latente segue distribuição logistica com parâmetros 0,1
-    F_Y10=plnorm((Y10),meanlog =exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2),
-                 sdlog =sqrt((exp(1.81^2)-1)*exp(2*exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2)+(1.81)^2)))
-    F_Y10[F_Y10==1]=0.9999999999
-    #hist(F_Y10)
-    F_inver_F_Y10=qlnorm(F_Y10,meanlog =exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2),
-                         sdlog = sqrt((exp(1.81^2)-1)*exp(2*exp(mean((Z_cov_mean-1)/2-2*1+(Z_cov_mean-1)*1/4)+(1.81)^2/2)+(1.81)^2)))
-    
     
     ##########################################################
     #####################RESULTADOS###########################
@@ -86,16 +81,32 @@ mc_function=function(N){
     tau_5=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+1])-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,q+1])
     tau_6=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+2])-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,q+2])
     
+    gamma_4=mean(matriz_Y[matriz_Y[,TT+1]==1,q])-mean(matriz_Y_naotratamento[matriz_Y_naotratamento[,TT+1]==1,q]) #resultados populacionais
+    gamma_5=mean(matriz_Y[matriz_Y[,TT+1]==1,q+1])-mean(matriz_Y_naotratamento[matriz_Y_naotratamento[,TT+1]==1,q+1])
+    gamma_6=mean(matriz_Y[matriz_Y[,TT+1]==1,q+2])-mean(matriz_Y_naotratamento[matriz_Y_naotratamento[,TT+1]==1,q+2])
+    
+    
     tau_4_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
     tau_5_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+1])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+1)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
     tau_6_hat=log(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,q+2])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+2)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
-
+    
+    
+    gamma_4_hat=exp(tau_4_hat+exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))-exp(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
+    gamma_5_hat=exp(tau_5_hat+exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+1)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))-exp(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+1)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
+    gamma_6_hat=exp(tau_6_hat+exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+2)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))-exp(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==1,(q-1)])))*(exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q+2)])))/exp(mean(log(matriz_Y[matriz_Y[,TT+1]==0,(q-1)])))))
+    
     matriz_resultados[p,1]=tau_4
     matriz_resultados[p,2]=tau_4_hat
     matriz_resultados[p,3]=tau_5
     matriz_resultados[p,4]=tau_5_hat
     matriz_resultados[p,5]=tau_6
     matriz_resultados[p,6]=tau_6_hat
+    matriz_resultados[p,7]=gamma_4
+    matriz_resultados[p,8]=gamma_4_hat
+    matriz_resultados[p,9]=gamma_5
+    matriz_resultados[p,10]=gamma_5_hat
+    matriz_resultados[p,11]=gamma_6
+    matriz_resultados[p,12]=gamma_6_hat
     
   }
   return(matriz_resultados)
