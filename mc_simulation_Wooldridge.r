2b15a84a3c2a7e9bada4268991cf415f556d6b54
wendtvi <vitoriawendt@gmail.com>
2023-03-05 09:12:03 -0300

    Update mc_simulation_Wooldridge.r

--- mc_simulation_Wooldridge.r
+++ mc_simulation_Wooldridge.r
@@ -13,19 +13,21 @@ vetor_mc_resultados_sd=vector()
 vetor_mc_resultados_media_pop=vector()
 vetor_mc_resultados_media_est=vector()
 vetor_mc_resultados_sd_vies=vector()
-
+vetor_mc_resultados_mcsd=vector()
 
 yvetor_mc_resultados_vies=vector()
 yvetor_mc_resultados_sd=vector()
 yvetor_mc_resultados_media_pop=vector()
 yvetor_mc_resultados_media_est=vector()
 yvetor_mc_resultados_sd_vies=vector()
+yvetor_mc_resultados_mcsd=vector()
 c=0
 for (k in seq(1,ncol(matriz_resultados)/2,2)){
   c=c+1
   vetor_mc_resultados_vies[c]=mean(matriz_resultados[,k])-mean(matriz_resultados[,k+1])
   vetor_mc_resultados_sd_vies[c]=mean(abs(matriz_resultados[,k]-(matriz_resultados[,k+1])))
-  vetor_mc_resultados_sd[c]=var(matriz_resultados[,k+1])*sqrt((kurtosis(matriz_resultados[,k+1])-1)/N)
+  vetor_mc_resultados_mcsd[c]=var(matriz_resultados[,k+1])*sqrt((kurtosis(matriz_resultados[,k+1])-1)/N)
+  vetor_mc_resultados_sd[c]=sd(matriz_resultados[,k+1])
   vetor_mc_resultados_media_pop[c]=mean(matriz_resultados[,k])
   vetor_mc_resultados_media_est[c]=mean(matriz_resultados[,k+1])
 }
@@ -35,7 +37,8 @@ for (k in seq(ncol(matriz_resultados)/2+1,ncol(matriz_resultados),2)){
   c=c+1
   yvetor_mc_resultados_vies[c]=mean(matriz_resultados[,k])-mean(matriz_resultados[,k+1])
   yvetor_mc_resultados_sd_vies[c]=mean(abs(matriz_resultados[,k]-(matriz_resultados[,k+1])))
-  yvetor_mc_resultados_sd[c]=var(matriz_resultados[,k+1])*sqrt((kurtosis(matriz_resultados[,k+1])-1)/N)
+  yvetor_mc_resultados_mcsd[c]=var(matriz_resultados[,k+1])*sqrt((kurtosis(matriz_resultados[,k+1])-1)/N)
+  yvetor_mc_resultados_sd[c]=sd(matriz_resultados[,k+1])
   yvetor_mc_resultados_media_pop[c]=mean(matriz_resultados[,k])
   yvetor_mc_resultados_media_est[c]=mean(matriz_resultados[,k+1])
 }
