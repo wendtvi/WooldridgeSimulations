9831a9cbbe107ee823797992fd972b4f64c6f118
wendtvi <vitoriawendt@gmail.com>
2023-03-06 10:59:30 -0300

    Update WOOLDRIDGE_discrete_nonlinear_woTimeInvariance.r

--- WOOLDRIDGE_discrete_nonlinear_woTimeInvariance.r
+++ WOOLDRIDGE_discrete_nonlinear_woTimeInvariance.r
@@ -2,7 +2,7 @@ mc_function=function(N){
   n1=500
   TT=6
   q=4
-  matriz_resultados=matrix(NA, ncol = 2*((TT-q+1)*2),nrow = N)
+  matriz_resultados=matrix(NA, ncol = 3*((TT-q+1)*2),nrow = N)
   
   for(p in 1:N){
     ##########################################################
@@ -34,8 +34,8 @@ mc_function=function(N){
     matriz_estado_naotratamento=matrix(NA,nrow = n1,ncol = TT+1)
     matriz_estado_naotratamento[,TT+1]=t(D)
     for (k in 1:TT){
-      if (k<q) matriz_estado_naotratamento[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,1,3)+ct_0>0)
-      if (k>=q) matriz_estado_naotratamento[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,1,3)+ct_0>0)
+      if (k<q) matriz_estado_naotratamento[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1)+ct_0>0)
+      if (k>=q) matriz_estado_naotratamento[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1)+ct_0>0)
     }
     
     #Gerando matriz de dados latentes.
@@ -43,7 +43,7 @@ mc_function=function(N){
     matriz_X_estrela[,TT+1]=t(D)
     for (k in 1:ncol(matriz_X_estrela)-1){
       matriz_X_estrela[,k]=matriz_estado_naotratamento[,k]
-      if (k<q) matriz_X_estrela[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,1,3)+ct_inf>0)
+      if (k<q) matriz_X_estrela[,k]=as.numeric(-(Z_cov_mean-1)/2-2*D+(Z_cov_mean-1)*D/4+rnorm(n1,0,1)+ct_inf>0)
       for (i in 1:nrow(matriz_X_estrela)){
         if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_mean[i]-1)-2*D[i]+rnorm(1,0,1)+ct_inf[i]>0)
         if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_mean[i]-1)-2*D[i]+0.2*f5[i]+rnorm(1,0,1)+ct_inf[i]>0)
@@ -59,16 +59,16 @@ mc_function=function(N){
     X_estrela_vetor_0_0=vector()
     X_estrela_vetor_1_0=vector()
     for (k in 1:TT){
-      X_estrela_vetor_0_0[k]=mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==0,k])/(1-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==0,k]))
-      X_estrela_vetor_1_0[k]=mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,k])/(1-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,k]))
+      X_estrela_vetor_0_0[k]=log(mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==0,k])/(1-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==0,k])))
+      X_estrela_vetor_1_0[k]=log(mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,k])/(1-mean(matriz_estado_naotratamento[matriz_estado_naotratamento[,TT+1]==1,k])))
     }
     
     #GERANDO VARIAVEL X
     X_vetor_0_0=vector()
     X_vetor_1_0=vector()
     for (k in 1:TT){
-      X_vetor_0_0[k]= X_estrela_vetor_0_0[k]/(1+ X_estrela_vetor_0_0[k])
-      X_vetor_1_0[k]=X_estrela_vetor_1_0[k]/(1+X_estrela_vetor_1_0[k])
+      X_vetor_0_0[k]=exp(X_estrela_vetor_0_0[k])/(1+exp(X_estrela_vetor_0_0[k]))
+      X_vetor_1_0[k]=exp(X_estrela_vetor_1_0[k])/(1+exp(X_estrela_vetor_1_0[k]))
     }
     
     
@@ -89,8 +89,8 @@ mc_function=function(N){
     X_estrela_vetor_0_inf=vector()
     X_estrela_vetor_1_inf=vector()
     for (k in 1:TT){
-      X_estrela_vetor_0_inf[k]=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,k])/(1-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,k]))
-      X_estrela_vetor_1_inf[k]=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,k])/(1-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,k]))
+      X_estrela_vetor_0_inf[k]=log(mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,k])/(1-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,k])))
+      X_estrela_vetor_1_inf[k]=log(mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,k])/(1-mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,k])))
     }
     
     
@@ -98,11 +98,11 @@ mc_function=function(N){
     X_vetor_0_inf=vector()
     X_vetor_1_inf=vector()
     for (k in 1:TT){
-      X_vetor_0_inf[k]=X_estrela_vetor_0_inf[k]/(1+X_estrela_vetor_0_inf[k])
-      X_vetor_1_inf[k]=X_estrela_vetor_1_inf[k]/(1+X_estrela_vetor_1_inf[k])
+      X_vetor_0_inf[k]=exp(X_estrela_vetor_0_inf[k])/(1+exp(X_estrela_vetor_0_inf[k]))
+      X_vetor_1_inf[k]=exp(X_estrela_vetor_1_inf[k])/(1+exp(X_estrela_vetor_1_inf[k]))
     }
     
-
+    
     #GERANDO VARIAVEL Y
     Y_vetor_0_inf=vector()
     Y_vetor_1_inf=vector()
@@ -124,15 +124,15 @@ mc_function=function(N){
     Ybar_01_t5=X_vetor_0_inf[q+1]
     Ybar_01_t6=X_vetor_0_inf[q+2]
     
-    G_Ybar_11_t4=X_vetor_1_inf[q]/(1+X_vetor_1_inf[q])
-    G_Ybar_11_t5=X_vetor_1_inf[q+1]/(1+X_vetor_1_inf[q+1])
-    G_Ybar_11_t6=X_vetor_1_inf[q+2]/(1+X_vetor_1_inf[q+2])
+    G_Ybar_11_t4=exp(X_estrela_vetor_1_inf[q])/(1+exp(X_estrela_vetor_1_inf[q]))
+    G_Ybar_11_t5=exp(X_estrela_vetor_1_inf[q+1])/(1+exp(X_estrela_vetor_1_inf[q+1]))
+    G_Ybar_11_t6=exp(X_estrela_vetor_1_inf[q+2])/(1+exp(X_estrela_vetor_1_inf[q+2]))
     
-    G_Ybar_10_t3=X_vetor_1_inf[q-1]/(1+X_vetor_1_inf[q-1])
-    G_Ybar_00_t3=X_vetor_0_inf[q-1]/(1+X_vetor_0_inf[q-1])
-    G_Ybar_01_t4=X_vetor_0_inf[q]/(1+X_vetor_0_inf[q])
-    G_Ybar_01_t5=X_vetor_0_inf[q+1]/(1+X_vetor_0_inf[q+1])
-    G_Ybar_01_t6=X_vetor_0_inf[q+2]/(1+X_vetor_0_inf[q+2])
+    G_Ybar_10_t3=exp(X_estrela_vetor_1_inf[q-1])/(1+exp(X_estrela_vetor_1_inf[q-1]))
+    G_Ybar_00_t3=exp(X_estrela_vetor_0_inf[q-1])/(1+exp(X_estrela_vetor_0_inf[q-1]))
+    G_Ybar_01_t4=exp(X_estrela_vetor_0_inf[q])/(1+exp(X_estrela_vetor_0_inf[q]))
+    G_Ybar_01_t5=exp(X_estrela_vetor_0_inf[q+1])/(1+exp(X_estrela_vetor_0_inf[q+1]))
+    G_Ybar_01_t6=exp(X_estrela_vetor_0_inf[q+2])/(1+exp(X_estrela_vetor_0_inf[q+2]))
     
     
     ##########################################################
@@ -142,18 +142,28 @@ mc_function=function(N){
     tau_5=X_estrela_vetor_1_inf[q+1]-X_estrela_vetor_1_0[q+1]
     tau_6=X_estrela_vetor_1_inf[q+2]-X_estrela_vetor_1_0[q+2]
     
-    tau_4_hat=X_estrela_vetor_1_inf[q]-((G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3))
-    tau_5_hat=X_estrela_vetor_1_inf[q+1]-((G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3))
-    tau_6_hat=X_estrela_vetor_1_inf[q+2]-((G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3))
+    tau_4_hat=X_estrela_vetor_1_inf[q]-log((G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3)))
+    tau_5_hat=X_estrela_vetor_1_inf[q+1]-log((G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3)))
+    tau_6_hat=X_estrela_vetor_1_inf[q+2]-log((G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3)))
+  
     
     gamma_4=X_vetor_1_inf[q]-X_vetor_1_0[q]
     gamma_5=X_vetor_1_inf[q+1]-X_vetor_1_0[q+1]
     gamma_6=X_vetor_1_inf[q+2]-X_vetor_1_0[q+2]
     
-    gamma4_hat=X_vetor_1_inf[q]-((G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3)))
-    gamma5_hat=X_vetor_1_inf[q+1]-((G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3)))
-    gamma6_hat=X_vetor_1_inf[q+2]-((G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3)/(1-(G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3)))
+    gamma4_hat=X_vetor_1_inf[q]-((G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3))
+    gamma5_hat=X_vetor_1_inf[q+1]-((G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3))
+    gamma6_hat=X_vetor_1_inf[q+2]-((G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3))
     
+    
+    kappa_4=Y_vetor_1_inf[q]-Y_vetor_1_0[q]
+    kappa_5=Y_vetor_1_inf[q+1]-Y_vetor_1_0[q+1]
+    kappa_6=Y_vetor_1_inf[q+2]-Y_vetor_1_0[q+2]
+    
+    
+    kappa4_hat=Y_vetor_1_inf[q]-((G_Ybar_01_t4-G_Ybar_00_t3+G_Ybar_10_t3)*(n1*pD))
+    kappa5_hat=Y_vetor_1_inf[q+1]-((G_Ybar_01_t5-G_Ybar_00_t3+G_Ybar_10_t3)*(n1*pD))
+    kappa6_hat=Y_vetor_1_inf[q+2]-((G_Ybar_01_t6-G_Ybar_00_t3+G_Ybar_10_t3)*(n1*pD))
     
     matriz_resultados[p,1]=tau_4
     matriz_resultados[p,2]=tau_4_hat
@@ -167,6 +177,12 @@ mc_function=function(N){
     matriz_resultados[p,10]=gamma5_hat
     matriz_resultados[p,11]=gamma_6
     matriz_resultados[p,12]=gamma6_hat
+    matriz_resultados[p,13]=kappa_4
+    matriz_resultados[p,14]=kappa4_hat
+    matriz_resultados[p,15]=kappa_5
+    matriz_resultados[p,16]=kappa5_hat
+    matriz_resultados[p,17]=kappa_6
+    matriz_resultados[p,18]=kappa6_hat
   }
   return(matriz_resultados)
 }
